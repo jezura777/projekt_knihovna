9c0fb52609249ce465cb4d65f9204524fc5791c6
Jezura777 <luki.fi@seznam.cz>
2026-05-25 22:18:15 +0200

    translated db to en

--- db.sql
+++ db.sql
@@ -1,40 +1,40 @@
 -- CREATE DATABASE knihovnadivnice;
 -- USE knihovnadivnice;
 
-CREATE TABLE IF NOT EXISTS autori (
+CREATE TABLE IF NOT EXISTS authors (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    jmeno VARCHAR(255) NOT NULL,
-    datum_narozeni DATE,
-    datum_umrti DATE
+    name VARCHAR(255) NOT NULL,
+    born DATE,
+    died DATE
 );
 
-CREATE TABLE IF NOT EXISTS knihy (
+CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nazev VARCHAR(100),
-    datum_vydani DATE,
-    autor_id INT,
-    pocet INT DEFAULT 1,
+    title VARCHAR(100),
+    published DATE,
+    author_id INT,
+    copies INT DEFAULT 1,
 
-    FOREIGN KEY(autor_id) REFERENCES autori(id)
+    FOREIGN KEY(author_id) REFERENCES authors(id)
 );
 
-CREATE TABLE IF NOT EXISTS uzivatele (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    jmeno VARCHAR(255) NOT NULL,
-    datum_narozeni DATE,
+    name VARCHAR(255) NOT NULL,
+    born DATE,
     email VARCHAR(255) UNIQUE
 );
 
 
-CREATE TABLE IF NOT EXISTS vypujcky (
+CREATE TABLE IF NOT EXISTS loans (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    uzivatel_id INT NOT NULL,
-    knizka_id INT NOT NULL,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
 
-    datum_zpracovani DATETIME,
-    datum_vypujceni DATE,
-    datum_vraceni DATE,
+    processed_at DATETIME,
+    loaned_on DATE,
+    due_on DATE,
 
-    FOREIGN KEY(uzivatel_id) REFERENCES uzivatele(id),
-    FOREIGN KEY(knizka_id) REFERENCES knihy(id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(book_id) REFERENCES book(id)
 );
